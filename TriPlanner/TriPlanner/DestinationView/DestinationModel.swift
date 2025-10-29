027359015352081512231a5c1b2f7fbc9388f685
Lilian <lilianguichard974@gmail.com>
2025-10-29 10:12:24 +0100

    fix image destinationModel

--- TriPlanner/TriPlanner/DestinationView/DestinationModel.swift
+++ TriPlanner/TriPlanner/DestinationView/DestinationModel.swift
@@ -5,13 +5,14 @@
 //  Created by apprenant87 on 28/10/2025.
 //
 
-import UIKit
+import Foundation
+import SwiftUI
 
 struct Destination : Identifiable {
     var id: UUID = UUID()
     var localisationCountry: String
     var localisationCity: String
-    var imageBonsPlans: UIImage?
+    var imageBonsPlans: Image
     var price: Double
     var adresse: String
     var rating: Double
