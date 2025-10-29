fde6b45678e3a72f1c6a701435d5fac1daf440bd
Rayane <rayaneelasraoui@gmail.com>
2025-10-29 16:53:37 +0100

    ajout tableau pour CalendarData

--- TriPlanner/TriPlanner/Data/CalendarData.swift
+++ TriPlanner/TriPlanner/Data/CalendarData.swift
@@ -8,3 +8,10 @@
 import Foundation
 
 let calendarTest: Calendar = Calendar(day: Date.now, hour: 16, minute: 23)
+
+let calendarTableTest: [Calendar] = [
+    Calendar(day: Date.now, hour: 15, minute: 45),
+    Calendar(day: Date(), hour: 23, minute: 18),
+    calendarTest,
+    Calendar(day: Date(), hour: 3, minute: 59)
+]
---
+++ TriPlanner/TriPlanner/Extracts/CalendarExtract.swift
@@ -0,0 +1,8 @@
+//
+//  CalendarExtract.swift
+//  TriPlanner
+//
+//  Created by apprenant86 on 29/10/2025.
+//
+
+import Foundation
