2a5d784786a2dda19d4be14ca2893be93490323c
Rayane <rayaneelasraoui@gmail.com>
2025-11-03 09:12:08 +0100

    creation interface calendrier

--- TriPlanner/TriPlanner/Extracts/CalendarExtract.swift
+++ TriPlanner/TriPlanner/Extracts/CalendarExtract.swift
@@ -2,7 +2,26 @@
 //  CalendarExtract.swift
 //  TriPlanner
 //
-//  Created by apprenant86 on 29/10/2025.
+//  Created by apprenant86 on 31/10/2025.
 //
-
 import Foundation
+import SwiftUI
+
+struct CalendarExtract: View {
+    @State private var selectedDate = Date()
+    @State private var dateRetour = Date()
+    
+    
+    var body: some View {
+        Form {
+            DatePicker("Date", selection: $selectedDate, displayedComponents: .date)
+                .datePickerStyle(.graphical)
+            DatePicker("Départ", selection: $selectedDate, displayedComponents: .hourAndMinute)
+            DatePicker("Retour", selection: $dateRetour)
+        }
+    }
+}
+
+#Preview {
+    CalendarExtract()
+}
